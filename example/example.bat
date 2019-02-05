3d1d37595c0fb60b2c78cc085cdfa82741339b28
tama <tama@ttk1.net>
2019-02-06 00:18:26 +0900

    実行方法変更。

--- example/example.bat
+++ example/example.bat
@@ -1,4 +1,5 @@
 set WSLENV=JAVA_HOME
 set PATH=%PATH%;C:\Users\%USERNAME%\hadoop\hadoop-3.2.0\bin
 set JAVA_HOME=/mnt/c/Users/%USERNAME%/hadoop/jdk-11.0.2
-bash
+bash example.sh
+pause
--- example/example.sh
+++ example/example.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
+
+cd $(dirname $0)
+
+if [ -d output ]; then
+  rm -r output
+fi
 
 hadoop jar WordCountMR-1.0-SNAPSHOT.jar input output
 cat output/*
-rm -r output
